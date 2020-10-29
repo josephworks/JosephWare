e2c1d3c24ccd9614b02fa2d04d825dbe2a50418e
Joseph Robinson <smartech56@gmail.com>
2020-10-28 19:29:34 -0700

    Made Authorization External

---
+++ JosephWare/Authorization.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace JosephWare
+{
+    class Authorization
+    {
+        static public void Login(string username, string password)
+        {
+            if (username == "joseph")
+            {
+                if (password == "joseph")
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Program.Joseph();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Password invalid");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Username not found!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
--- JosephWare/Program.cs
+++ JosephWare/Program.cs
@@ -14,7 +14,7 @@ namespace JosephWare
             Write("Password: ");
             string password = ReadLine();
 
-            Login(username, password);
+            Authorization.Login(username, password);
         }
 
         public static void Joseph()
@@ -34,30 +34,6 @@ namespace JosephWare
                 default:
                     WriteLine("Command not found!");
                     break;
-            }
-        }
-
-        public static void Login(string username, string password)
-        {
-            if (username == "joseph")
-            {
-                if (password == "joseph")
-                {
-                    BackgroundColor = ConsoleColor.Blue;
-                    Joseph();
-                }
-                else
-                {
-                    ForegroundColor = ConsoleColor.Red;
-                    WriteLine("Password invalid");
-                    ReadKey();
-                }
-            }
-            else
-            {
-                ForegroundColor = ConsoleColor.Red;
-                WriteLine("Username not found!");
-                ReadKey();
             }
         }
     }
