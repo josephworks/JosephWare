289f6c78cc37e9322163567c3bcbef495ad1e8df
Joseph Robinson <smartech56@gmail.com>
2020-10-28 20:24:46 -0700

    General Updates

--- JosephWare/Program.cs
+++ JosephWare/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Net.Mime;
-using System.Windows.Forms;
+using System.Threading;
 using Figgle;
 using JosephWare.gui;
 using static System.Console;
@@ -13,15 +12,32 @@ namespace JosephWare
         static void Main(string[] args)
         {
             WriteLine(FiggleFonts.ThreePoint.Render("JosephWare"));
-            Write("Username: ");
-            string username = ReadLine();
-            Write("Password: ");
-            string password = ReadLine();
 
-            Authorization.Login(username, password);
+            // Check if ESC is pressed while attempting to log in
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Press ESC to continue as guest");
+            ResetColor();
+
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    Write("Username: ");
+                    string username = ReadLine();
+                    Write("Password: ");
+                    string password = ReadLine();
+
+                    Authorization.Login(username, password);
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            // Continue as guest if ESC is pressed
+            WriteLine("Wheel just test it out");
+            Console.WriteLine("Wheel just test it out");
         }
 
-        public static void Joseph()
+        static public void Joseph()
         {
             Console.Beep();
             Console.BackgroundColor = ConsoleColor.Blue;
