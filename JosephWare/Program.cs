ab959c81e4ad6c63bf624ac4302837943968d30e
Joseph Robinson <smartech56@gmail.com>
2020-10-28 19:19:28 -0700

    Added code

---
+++ JosephWare/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Figgle;
+using static System.Console;
+
+namespace JosephWare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(FiggleFonts.ThreePoint.Render("JosephWare"));
+            Write("Username: ");
+            string username = ReadLine();
+            Write("Password: ");
+            string password = ReadLine();
+
+            Login(username, password);
+        }
+
+        public static void Joseph()
+        {
+            Console.Beep();
+            WriteLine("Logged in successfully!");
+            Write("JConsole> ");
+            string command = ReadLine().ToLower();
+            switch (command)
+            {
+                case "help":
+                    WriteLine("Case 1");
+                    break;
+                case "exit":
+                    WriteLine("Case 2");
+                    break;
+                default:
+                    WriteLine("Command not found!");
+                    break;
+            }
+        }
+
+        public static void Login(string username, string password)
+        {
+            if (username == "joseph")
+            {
+                if (password == "joseph")
+                {
+                    BackgroundColor = ConsoleColor.Blue;
+                    Joseph();
+                }
+                else
+                {
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("Password invalid");
+                    ReadKey();
+                }
+            }
+            else
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine("Username not found!");
+                ReadKey();
+            }
+        }
+    }
+}
